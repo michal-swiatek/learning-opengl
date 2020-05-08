784b411d538ceb4252a0632d9874c472368b89d6
michal-swiatek <swiatek.michal006@gmail.com>
2020-05-08 09:38:34 +0200

    Moved creating window into subproject

--- main.cpp
+++ main.cpp
@@ -10,35 +10,5 @@ int main(int argc, char** argv) {
         // Report version
         std::cout << "Version " << learning_opengl_VERSION_MAJOR << '.' << learning_opengl_VERSION_MINOR << std::endl;
 
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    glViewport(0, 0, 800, 600);
-
-    while(!glfwWindowShouldClose(window))
-    {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-
     return 0;
 }
