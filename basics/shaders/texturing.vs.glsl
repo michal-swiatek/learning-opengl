75c873bd637d97854fb2d685d0f36084facc0e5b
michal-swiatek <swiatek.michal006@gmail.com>
2020-05-14 01:39:16 +0200

    Changed a- prefix to v- (vertex)

--- basics/shaders/texturing.vs.glsl
+++ basics/shaders/texturing.vs.glsl
@@ -1,15 +1,15 @@
 #version 330
 
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec2 aTexCoords;
+layout (location = 0) in vec3 vPos;
+layout (location = 1) in vec3 vColor;
+layout (location = 2) in vec2 vTexCoords;
 
 out vec3 color;
 out vec2 texCoords;
 
 void main() {
-    gl_Position = vec4(aPos, 1.0);
+    color = vColor;
+    texCoords = vTexCoords;
 
-    color = aColor;
-    texCoords = aTexCoords;
+    gl_Position = vec4(vPos, 1.0);
 }
