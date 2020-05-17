40ff7cb090c5077e4ac9ed4620588fce0c271e28
michal-swiatek <swiatek.michal006@gmail.com>
2020-05-17 20:35:35 +0200

    Added cylinder generation and rendering example

---
+++ examples/cylinder/cylinder.cpp
@@ -0,0 +1,93 @@
+/*
+ *  Created by michal-swiatek on 17.05.2020.
+ *  Github: https://github.com/michal-swiatek/learning-opengl
+ *
+ *  Cylinder generation by Rafał Uzarowicz
+ */
+
+#include "OpenGLApp.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "Cylinder.h"
+#include "Shader.h"
+
+const int WIDTH = 1280;
+const int HEIGHT = 720;
+
+class CylinderVisualization : public OpenGLApp
+{
+private:
+    std::unique_ptr<Shader> cylinderShader;
+    std::unique_ptr<Cylinder> cylinder;
+
+    uint VBO, VAO, EBO;
+
+public:
+    CylinderVisualization() : OpenGLApp("Cylinder visualtization") { }
+
+    void init() override
+    {
+        cylinder = std::make_unique<Cylinder>(0.5, 0.5, 10.0, 30, 30);
+
+        glGenBuffers(1, &VBO);
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, cylinder->vertices.size() * sizeof(float), cylinder->vertices.data(), GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, cylinder->indices.size() * sizeof(unsigned int), cylinder->indices.data(), GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(0));
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+
+        glBindVertexArray(0);
+
+        cylinderShader = std::make_unique<Shader>("shaders/phong_view.vs.glsl", "shaders/phong_view.fs.glsl");
+
+        cylinderShader->use();
+        cylinderShader->setVector3f("lightColor", glm::vec3(1.0f));
+        cylinderShader->setVector3f("ambientColor", glm::vec3(0.5f));
+        cylinderShader->setVector3f("diffuseColor", glm::vec3(1.0f));
+        cylinderShader->setVector3f("specularColor", glm::vec3(0.6f));
+    }
+
+    void draw() override
+    {
+        glm::mat4 projection = glm::perspective(camera.getZoom(), float(WIDTH)/float(HEIGHT), 0.1f, 100.0f);
+
+        glm::mat4 view = camera.getViewMatrix();
+
+        cylinderShader->use();
+        cylinderShader->setVector3f("lightPos", glm::vec3(view * glm::vec4(glm::vec3(10.0f), 1.0f)));
+
+        cylinderShader->setMatrix4f("mv", view);
+        cylinderShader->setMatrix4f("mvp", projection * view);
+        cylinderShader->setMatrix3f("mvInvTrans", glm::mat3(glm::transpose(glm::inverse(view))));
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, cylinder->indices.size(), GL_UNSIGNED_INT, 0);
+    }
+};
+
+int main() {
+    CylinderVisualization app;
+    app.initApp(WIDTH, HEIGHT, false, false);
+
+    app.run();
+
+    return 0;
+}
+
+
--- utility/include/Cylinder.h
+++ utility/include/Cylinder.h
@@ -13,12 +13,12 @@ public:
 	int sectorsNum;
 	int stacksNum;
 
-	const float PI = acos(-1);
-	unsigned int paramNum = 3;
+	const float PI = (float)acos(-1);
+	unsigned int paramNum = 8;
 
 public:
-	Cylinder( float bR = 1.0f, float tR = 1.0f, float h = 2.0f, int secNum = 4, int staNum = 3 ){
-		setParams(bR, tR, h, secNum, staNum);
+	Cylinder(float bR = 1.0f, float tR = 1.0f, float h = 2.0f, int secNum = 4, int staNum = 3) {
+		this->setParams(bR, tR, h, secNum, staNum);
 	}
 
 	void setParams(float bR, float tR, float h, int secNum, int staNum) {
@@ -40,12 +40,11 @@ public:
 			this->height = h;
 			this->sectorsNum = secNum;
 			this->stacksNum = staNum;
+
+			this->clearVectors();
+
+			this->generateCylinder();
 		}
-
-		clearVectors();
-
-		generateCylinder();
-		
 	}
 
 	std::vector<float> getVertices() {
@@ -62,114 +61,203 @@ public:
 	}
 
 	void generateCylinder() {
-		generateVertices();
-		generateIndices();
+		this->generateVertices();
+		this->generateIndices();
+	}
+
+	float vectorLength(std::vector<float> vec) {
+		float squaresSum = 0.0f;
+		for (int i = 0; i < vec.size(); ++i) {
+			squaresSum += vec[i] * vec[i];
+		}
+		return sqrt(squaresSum);
 	}
 
 	std::vector<float> generateBaseCircleVertices() {
-		float angleStep = 2 * PI / sectorsNum;
+		float angleStep = 2 * PI / this->sectorsNum;
 
 		std::vector<float> circleVertices;
+		// Coords
+		circleVertices.push_back(0.0f);
+		circleVertices.push_back(-this->height / 2.0f);
+		circleVertices.push_back(0.0f);
+		// N vec coords
+		circleVertices.push_back(0.0f);
+		circleVertices.push_back(-1.0f);
+		circleVertices.push_back(0.0f);
+		// UV
 		circleVertices.push_back(0.0f);
 		circleVertices.push_back(0.0f);
-		circleVertices.push_back(-height / 2.0f);
-		if (baseRadius > 0) {
-			for (int i = 0; i < sectorsNum; ++i) {
-				circleVertices.push_back(topRadius*cos(i*angleStep));
-				circleVertices.push_back(topRadius*sin(i*angleStep));
-				circleVertices.push_back(-height / 2.0f);
+		if (this->baseRadius > 0) {
+			for (int i = 0; i < this->sectorsNum; ++i) {
+				// Coords
+				circleVertices.push_back(this->baseRadius*cos(i*angleStep));
+				circleVertices.push_back(-this->height / 2.0f);
+				circleVertices.push_back(this->baseRadius*sin(i*angleStep));
+				// N vec coords
+				std::vector<float> nVec = { this->baseRadius*cos(i*angleStep), 0.0f, this->baseRadius*sin(i*angleStep) };
+				float nVecLen = vectorLength(nVec);
+				for (int i = 0; i < nVec.size(); ++i) {
+					nVec[i] /= nVecLen;
+				}
+
+				circleVertices.push_back(nVec[0]);
+				circleVertices.push_back(nVec[1]);
+				circleVertices.push_back(nVec[2]);
+				// UV
+				circleVertices.push_back(0.0f);
+				circleVertices.push_back(0.0f);
 			}
 		}
 		return circleVertices;
 	}
+
 	std::vector<float> generateTopCircleVertices() {
-		float angleStep = 2 * PI / sectorsNum;
+		float angleStep = 2 * PI / this->sectorsNum;
 
 		std::vector<float> circleVertices;
+		// Coords
+		circleVertices.push_back(0.0f);
+		circleVertices.push_back(this->height / 2.0f);
+		circleVertices.push_back(0.0f);
+		// N vec coords
+		circleVertices.push_back(0.0f);
+		circleVertices.push_back(1.0f);
+		circleVertices.push_back(0.0f);
+		// UV
 		circleVertices.push_back(0.0f);
 		circleVertices.push_back(0.0f);
-		circleVertices.push_back(height / 2.0f);
-		if (topRadius > 0.0f) {
-			for (int i = 0; i < sectorsNum; ++i) {
-				circleVertices.push_back(topRadius*cos(i*angleStep));
-				circleVertices.push_back(topRadius*sin(i*angleStep));
-				circleVertices.push_back(height / 2.0f);
+		if (this->topRadius > 0.0f) {
+			for (int i = 0; i < this->sectorsNum; ++i) {
+				// Coords
+				circleVertices.push_back(this->topRadius*cos(i*angleStep));
+				circleVertices.push_back(this->height / 2.0f);
+				circleVertices.push_back(this->topRadius*sin(i*angleStep));
+				// N vec coords
+				std::vector<float> nVec = { this->topRadius*cos(i*angleStep), 0.0f, this->topRadius*sin(i*angleStep) };
+				float nVecLen = vectorLength(nVec);
+				for (int i = 0; i < nVec.size(); ++i) {
+					nVec[i] /= nVecLen;
+				}
+				circleVertices.push_back(nVec[0]);
+				circleVertices.push_back(nVec[1]);
+				circleVertices.push_back(nVec[2]);
+				// UV
+				circleVertices.push_back(0.0f);
+				circleVertices.push_back(0.0f);
 			}
 		}
 		return circleVertices;
 	}
+
 	void generateVertices() {
 		std::vector<float> topCircleVertices = generateTopCircleVertices();
 		std::vector<float> baseCircleVertices = generateBaseCircleVertices();
-		
+
 		// Add top circle vertices
-		for (int i = 0; i < topCircleVertices.size(); ++i) {
+		for (unsigned int i = 0; i < topCircleVertices.size(); ++i) {
 			this->vertices.push_back(topCircleVertices[i]);
 		}
 
 		// Count steps for each coords.
 		std::vector<std::vector<float>> verticesStep;
-		if (topRadius <= 0.0f) {
-			for (int i = 0; i < sectorsNum; ++i) {
+		if (this->topRadius <= 0.0f) {
+			for (int i = 0; i < this->sectorsNum; ++i) {
 				verticesStep.push_back(std::vector<float>());
-				verticesStep[i].push_back((baseCircleVertices[(i + 1) * 3] - topCircleVertices[0]) / stacksNum);
-				verticesStep[i].push_back((baseCircleVertices[(i + 1) * 3 + 1] - topCircleVertices[1]) / stacksNum);
-				verticesStep[i].push_back((baseCircleVertices[(i + 1) * 3 + 2] - topCircleVertices[2]) / stacksNum);
+				verticesStep[i].push_back((baseCircleVertices[(i + 1) * paramNum] - topCircleVertices[0]) / this->stacksNum);
+				verticesStep[i].push_back((baseCircleVertices[(i + 1) * paramNum + 1] - topCircleVertices[1]) / this->stacksNum);
+				verticesStep[i].push_back((baseCircleVertices[(i + 1) * paramNum + 2] - topCircleVertices[2]) / this->stacksNum);
 			}
 		}
-		else if (baseRadius <= 0.0f) {
-			for (int i = 0; i < sectorsNum; ++i) {
+		else if (this->baseRadius <= 0.0f) {
+			for (int i = 0; i < this->sectorsNum; ++i) {
 				verticesStep.push_back(std::vector<float>());
-				verticesStep[i].push_back((baseCircleVertices[0] - topCircleVertices[(i + 1) * 3]) / stacksNum);
-				verticesStep[i].push_back((baseCircleVertices[1] - topCircleVertices[(i + 1) * 3 + 1]) / stacksNum);
-				verticesStep[i].push_back((baseCircleVertices[2] - topCircleVertices[(i + 1) * 3 + 2]) / stacksNum);
+				verticesStep[i].push_back((baseCircleVertices[0] - topCircleVertices[(i + 1) * paramNum]) / this->stacksNum);
+				verticesStep[i].push_back((baseCircleVertices[1] - topCircleVertices[(i + 1) * paramNum + 1]) / this->stacksNum);
+				verticesStep[i].push_back((baseCircleVertices[2] - topCircleVertices[(i + 1) * paramNum + 2]) / this->stacksNum);
 			}
 		}
 		else {
-			for (int i = 0; i < sectorsNum; ++i) {
+			for (int i = 0; i < this->sectorsNum; ++i) {
 				verticesStep.push_back(std::vector<float>());
-				verticesStep[i].push_back((baseCircleVertices[(i + 1) * 3] - topCircleVertices[(i + 1) * 3]) / stacksNum);
-				verticesStep[i].push_back((baseCircleVertices[(i + 1) * 3 + 1] - topCircleVertices[(i + 1) * 3 + 1]) / stacksNum);
-				verticesStep[i].push_back((baseCircleVertices[(i + 1) * 3 + 2] - topCircleVertices[(i + 1) * 3 + 2]) / stacksNum);
+				verticesStep[i].push_back((baseCircleVertices[(i + 1) * paramNum] - topCircleVertices[(i + 1) * paramNum]) / this->stacksNum);
+				verticesStep[i].push_back((baseCircleVertices[(i + 1) * paramNum + 1] - topCircleVertices[(i + 1) * paramNum + 1]) / this->stacksNum);
+				verticesStep[i].push_back((baseCircleVertices[(i + 1) * paramNum + 2] - topCircleVertices[(i + 1) * paramNum + 2]) / this->stacksNum);
 			}
 		}
 
 		// Add between vertices
-		if (topRadius <= 0.0f) {
-			for (int i = 0; i < stacksNum - 1; ++i) {
-				for (int j = 0; j < sectorsNum; ++j) {
+		if (this->topRadius <= 0.0f) {
+			for (int i = 0; i < this->stacksNum; ++i) {
+				for (int j = 0; j < this->sectorsNum; ++j) {
+					// Coords
 					this->vertices.push_back(topCircleVertices[0] + (i + 1)*verticesStep[j][0]);
 					this->vertices.push_back(topCircleVertices[1] + (i + 1)*verticesStep[j][1]);
 					this->vertices.push_back(topCircleVertices[2] + (i + 1)*verticesStep[j][2]);
+					// N vec coords
+					std::vector<float> nVec = { topCircleVertices[0] + (i + 1)*verticesStep[j][0], 0.0f, topCircleVertices[2] + (i + 1)*verticesStep[j][2] };
+					float nVecLen = vectorLength(nVec);
+					for (int i = 0; i < nVec.size(); ++i) {
+						nVec[i] /= nVecLen;
+					}
+					this->vertices.push_back(nVec[0]);
+					this->vertices.push_back(nVec[1]);
+					this->vertices.push_back(nVec[2]);
+					// UV
+					this->vertices.push_back(0.0f);
+					this->vertices.push_back(0.0f);
 				}
 			}
 		}
-		else if (baseRadius <= 0.0f) {
-			for (int i = 0; i < stacksNum - 2; ++i) {
-				for (int j = 0; j < sectorsNum; ++j) {
-					this->vertices.push_back(topCircleVertices[(j + 1) * 3] + (i + 1)*verticesStep[j][0]);
-					this->vertices.push_back(topCircleVertices[(j + 1) * 3 + 1] + (i + 1)*verticesStep[j][1]);
-					this->vertices.push_back(topCircleVertices[(j + 1) * 3 + 2] + (i + 1)*verticesStep[j][2]);
+		else if (this->baseRadius <= 0.0f) {
+			for (int i = 0; i < this->stacksNum - 1; ++i) {
+				for (int j = 0; j < this->sectorsNum; ++j) {
+					// Coords
+					this->vertices.push_back(topCircleVertices[(j + 1) * paramNum] + (i + 1)*verticesStep[j][0]);
+					this->vertices.push_back(topCircleVertices[(j + 1) * paramNum + 1] + (i + 1)*verticesStep[j][1]);
+					this->vertices.push_back(topCircleVertices[(j + 1) * paramNum + 2] + (i + 1)*verticesStep[j][2]);
+					// N vec coords
+					std::vector<float> nVec = { topCircleVertices[(j + 1) * paramNum] + (i + 1)*verticesStep[j][0], 0.0f, topCircleVertices[(j + 1) * paramNum + 2] + (i + 1)*verticesStep[j][2] };
+					float nVecLen = vectorLength(nVec);
+					for (int i = 0; i < nVec.size(); ++i) {
+						nVec[i] /= nVecLen;
+					}
+					this->vertices.push_back(nVec[0]);
+					this->vertices.push_back(nVec[1]);
+					this->vertices.push_back(nVec[2]);
+					// UV
+					this->vertices.push_back(0.0f);
+					this->vertices.push_back(0.0f);
 				}
 			}
 		}
 		else {
-			for (int i = 0; i < stacksNum - 1; ++i) {
-				for (int j = 0; j < sectorsNum; ++j) {
-					this->vertices.push_back(topCircleVertices[(j + 1) * 3] + (i + 1)*verticesStep[j][0]);
-					this->vertices.push_back(topCircleVertices[(j + 1) * 3 + 1] + (i + 1)*verticesStep[j][1]);
-					this->vertices.push_back(topCircleVertices[(j + 1) * 3 + 2] + (i + 1)*verticesStep[j][2]);
+			for (int i = 0; i < this->stacksNum; ++i) {
+				for (int j = 0; j < this->sectorsNum; ++j) {
+					// Coords
+					this->vertices.push_back(topCircleVertices[(j + 1) * paramNum] + (i + 1)*verticesStep[j][0]);
+					this->vertices.push_back(topCircleVertices[(j + 1) * paramNum + 1] + (i + 1)*verticesStep[j][1]);
+					this->vertices.push_back(topCircleVertices[(j + 1) * paramNum + 2] + (i + 1)*verticesStep[j][2]);
+					// N vec coords
+					std::vector<float> nVec = { topCircleVertices[(j + 1) * paramNum] + (i + 1)*verticesStep[j][0], 0.0f, topCircleVertices[(j + 1) * paramNum + 2] + (i + 1)*verticesStep[j][2] };
+					float nVecLen = vectorLength(nVec);
+					for (int i = 0; i < nVec.size(); ++i) {
+						nVec[i] /= nVecLen;
+					}
+					this->vertices.push_back(nVec[0]);
+					this->vertices.push_back(nVec[1]);
+					this->vertices.push_back(nVec[2]);
+					// UV
+					this->vertices.push_back(0.0f);
+					this->vertices.push_back(0.0f);
 				}
 			}
 		}
 
-		// Add base circle vertices
-		//for (int i = 3; i < baseCircleVertices.size(); ++i) {
-		//	vertices.push_back(baseCircleVertices[i]);
-		//}
-		this->vertices.push_back(baseCircleVertices[0]);
-		this->vertices.push_back(baseCircleVertices[1]);
-		this->vertices.push_back(baseCircleVertices[2]);
+		// Add base vertices
+		for (int i = 0; i < paramNum; ++i) {
+			this->vertices.push_back(baseCircleVertices[i]);
+		}
 	}
 
 	void generateIndices() {
@@ -196,81 +284,81 @@ public:
 			this->indices.push_back(0);
 			this->indices.push_back(1);
 			this->indices.push_back(this->sectorsNum);
-			for (int i = 1; i < stacksNum - 1; ++i) {
-				for (int j = 0; j < sectorsNum - 1; ++j) {
-					this->indices.push_back(j + 1 + i * sectorsNum);
-					this->indices.push_back(j + 2 + i * sectorsNum);
-					this->indices.push_back(j + 1 + (i + 1) * sectorsNum);
-					this->indices.push_back(j + 2 + i * sectorsNum);
-					this->indices.push_back(j + 2 + (i + 1) * sectorsNum);
-					this->indices.push_back(j + 1 + (i + 1) * sectorsNum);
+			for (int i = 0; i < this->stacksNum - 1; ++i) {
+				for (int j = 0; j < this->sectorsNum - 1; ++j) {
+					this->indices.push_back(j + 1 + i * this->sectorsNum);
+					this->indices.push_back(j + 2 + i * this->sectorsNum);
+					this->indices.push_back(j + 1 + (i + 1) * this->sectorsNum);
+					this->indices.push_back(j + 2 + i * this->sectorsNum);
+					this->indices.push_back(j + 2 + (i + 1) * this->sectorsNum);
+					this->indices.push_back(j + 1 + (i + 1) * this->sectorsNum);
 				}
-				this->indices.push_back((i + 1)*sectorsNum);
-				this->indices.push_back(1 + i * sectorsNum);
-				this->indices.push_back((i + 2)*sectorsNum);
-				this->indices.push_back(1 + i * sectorsNum);
-				this->indices.push_back(1 + sectorsNum + i * sectorsNum);
-				this->indices.push_back((i + 2)*sectorsNum);
+				this->indices.push_back((i + 1)*this->sectorsNum);
+				this->indices.push_back(1 + i * this->sectorsNum);
+				this->indices.push_back((i + 2)*this->sectorsNum);
+				this->indices.push_back(1 + i * this->sectorsNum);
+				this->indices.push_back(1 + (i + 1) * this->sectorsNum);
+				this->indices.push_back((i + 2)*this->sectorsNum);
 			}
 		}
 		else if (this->baseRadius <= 0.0f) {
-			for (int i = 0; i < stacksNum - 2; ++i) {
-				for (int j = 0; j < sectorsNum - 1; ++j) {
-					this->indices.push_back(j + 1 + i * sectorsNum);
-					this->indices.push_back(j + 2 + i * sectorsNum);
-					this->indices.push_back(j + 1 + (i + 1) * sectorsNum);
-					this->indices.push_back(j + 2 + i * sectorsNum);
-					this->indices.push_back(j + 2 + (i + 1) * sectorsNum);
-					this->indices.push_back(j + 1 + (i + 1) * sectorsNum);
+			for (int i = 0; i < this->stacksNum - 1; ++i) {
+				for (int j = 0; j < this->sectorsNum - 1; ++j) {
+					this->indices.push_back(j + 1 + i * this->sectorsNum);
+					this->indices.push_back(j + 2 + i * this->sectorsNum);
+					this->indices.push_back(j + 1 + (i + 1) * this->sectorsNum);
+					this->indices.push_back(j + 2 + i * this->sectorsNum);
+					this->indices.push_back(j + 2 + (i + 1) * this->sectorsNum);
+					this->indices.push_back(j + 1 + (i + 1) * this->sectorsNum);
 				}
-				this->indices.push_back((i + 1)*sectorsNum);
-				this->indices.push_back(1 + i * sectorsNum);
-				this->indices.push_back((i + 2)*sectorsNum);
-				this->indices.push_back(1 + i * sectorsNum);
-				this->indices.push_back(1 + sectorsNum + i * sectorsNum);
-				this->indices.push_back((i + 2)*sectorsNum);
+				this->indices.push_back((i + 1)*this->sectorsNum);
+				this->indices.push_back(1 + i * this->sectorsNum);
+				this->indices.push_back((i + 2)*this->sectorsNum);
+				this->indices.push_back(1 + i * this->sectorsNum);
+				this->indices.push_back(1 + (i + 1) * this->sectorsNum);
+				this->indices.push_back((i + 2)*this->sectorsNum);
 			}
 			for (int i = 0; i < this->sectorsNum - 1; ++i) {
-				this->indices.push_back(this->vertices.size() / 3 - 1);
-				this->indices.push_back(this->vertices.size() / 3 - 3 - i);
-				this->indices.push_back(this->vertices.size() / 3 - 2 - i);
+				this->indices.push_back(this->vertices.size() / paramNum - 1);
+				this->indices.push_back(this->vertices.size() / paramNum - 3 - i);
+				this->indices.push_back(this->vertices.size() / paramNum - 2 - i);
 			}
-			this->indices.push_back(this->vertices.size() / 3 - 1);
-			this->indices.push_back(this->vertices.size() / 3 - 2);
-			this->indices.push_back(this->vertices.size() / 3 - 1 - this->sectorsNum);
+			this->indices.push_back(this->vertices.size() / paramNum - 1);
+			this->indices.push_back(this->vertices.size() / paramNum - 2);
+			this->indices.push_back(this->vertices.size() / paramNum - 1 - this->sectorsNum);
 		}
 		else {
-			for (int i = 0; i < stacksNum - 1; ++i) {
-				for (int j = 0; j < sectorsNum - 1; ++j) {
-					this->indices.push_back(j + 1 + i * sectorsNum);
-					this->indices.push_back(j + 2 + i * sectorsNum);
-					this->indices.push_back(j + 1 + (i+1) * sectorsNum);
-					this->indices.push_back(j + 2 + i * sectorsNum);
-					this->indices.push_back(j + 2 + (i+1) * sectorsNum);
-					this->indices.push_back(j + 1 + (i+1) * sectorsNum);
+			for (int i = 0; i < this->stacksNum; ++i) {
+				for (int j = 0; j < this->sectorsNum - 1; ++j) {
+					this->indices.push_back(j + 1 + i * this->sectorsNum);
+					this->indices.push_back(j + 2 + i * this->sectorsNum);
+					this->indices.push_back(j + 1 + (i + 1) * this->sectorsNum);
+					this->indices.push_back(j + 2 + i * this->sectorsNum);
+					this->indices.push_back(j + 2 + (i + 1) * this->sectorsNum);
+					this->indices.push_back(j + 1 + (i + 1) * this->sectorsNum);
 				}
-				this->indices.push_back((i + 1)*sectorsNum);
-				this->indices.push_back(1 + i * sectorsNum);
-				this->indices.push_back((i + 2)*sectorsNum);
-				this->indices.push_back(1 + i * sectorsNum);
-				this->indices.push_back(1 + sectorsNum + i * sectorsNum);
-				this->indices.push_back((i + 2)*sectorsNum);
+				this->indices.push_back((i + 1)*this->sectorsNum);
+				this->indices.push_back(1 + i * this->sectorsNum);
+				this->indices.push_back((i + 2)*this->sectorsNum);
+				this->indices.push_back(1 + i * this->sectorsNum);
+				this->indices.push_back(1 + (i + 1) * this->sectorsNum);
+				this->indices.push_back((i + 2)*this->sectorsNum);
 			}
 		}
 
 		// Base
 		if (this->baseRadius > 0.0f) {
 			for (int i = 0; i < this->sectorsNum - 1; ++i) {
-				this->indices.push_back(this->vertices.size()/3 - 1);
-				this->indices.push_back(this->vertices.size()/3 - 3 - i);
-				this->indices.push_back(this->vertices.size()/3 - 2 - i);
+				this->indices.push_back(this->vertices.size() / paramNum - 1);
+				this->indices.push_back(this->vertices.size() / paramNum - 3 - i);
+				this->indices.push_back(this->vertices.size() / paramNum - 2 - i);
 			}
-			this->indices.push_back(this->vertices.size()/3 - 1);
-			this->indices.push_back(this->vertices.size()/3 - 2);
-			this->indices.push_back(this->vertices.size()/3 - 1 - this->sectorsNum);
+			this->indices.push_back(this->vertices.size() / paramNum - 1);
+			this->indices.push_back(this->vertices.size() / paramNum - 2);
+			this->indices.push_back(this->vertices.size() / paramNum - 1 - this->sectorsNum);
 		}
 	}
 
-	
+
 };
 
